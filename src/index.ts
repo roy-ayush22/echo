afa9d0fc46a4004191154a118c74ec1261eb8895
roy-ayush22 <ayush30085@gmail.com>
2025-08-15 12:04:50 +0530

    ping-pong logic

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
 import { WebSocketServer } from "ws";
-
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (socket) => {
   console.log("user connected");
 
-  setInterval(() => {
-    socket.send("message from the server");
-  }, 1000);
-
-  socket.on("message", (data) => {
-    console.log(data.toString());
-  });
-
-  socket.on("close", () => {
-    console.log("user disconnected");
+  socket.on("message", (event) => {
+    if (event.toString() === "ping") {
+      socket.send("pong");
+    }
   });
 });
