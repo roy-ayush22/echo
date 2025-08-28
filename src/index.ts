2fcf94a6f70a98f41b9426a61990fc0d26b89ccf
roy-ayush22 <ayush30085@gmail.com>
2025-08-28 20:21:05 +0530

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import { WebSocketServer } from "ws";
+// import { ChatMessage, ClientMessage } from "../interface/interface";
+
 const wss = new WebSocketServer({ port: 8080 });
+const client = new Set<any>();
 
 wss.on("connection", (socket) => {
-  console.log("user connected");
+  client.add(socket);
+  console.log(`client connected, total clients: ${client.size}`);
 
-  socket.on("message", (event) => {
-    if (event.toString() === "ping") {
-      socket.send("pong");
-    }
+  socket.on("message", (data) => {
+    console.log(data.toString());
+  });
+
+  socket.send("message from server");
+
+  socket.on("close", () => {
+    client.delete(socket);
+    console.log(`client disconnected!`);
   });
 });
