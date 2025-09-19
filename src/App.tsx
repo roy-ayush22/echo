b2c5b220a3a08ba1aa4232bdf09a9e4063b7a1d8
roy-ayush22 <ayush30085@gmail.com>
2025-09-19 23:42:48 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p className='text-red-500'>hello there</p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/hooks/useWebSocket.ts
@@ -0,0 +1,2 @@
+import { WebSocket, WebSocketServer } from "ws";
+import { v4 as uuidv4 } from "uuid";
