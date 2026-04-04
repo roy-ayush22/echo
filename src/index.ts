c378a22a2121fe746bd1649119224717c60b63dc
roy-ayush22 <ayush30085@gmail.com>
2026-04-05 01:39:25 +0530

    implementing room logic

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ const wss = new WebSocketServer({ port: 8080 });
 interface Client {
   id: string;
   socket: WebSocket;
+  roomId: string | null;
 }
 
+const rooms: Map<string, Set<string>> = new Map();
 const clients: Map<string, Client> = new Map();
 
 const generateId = () => {
@@ -24,7 +26,7 @@ const broadcast = (message: string, excludeId?: string) => {
 
 wss.on("connection", (socket: WebSocket) => {
   const id = generateId();
-  const client: Client = { id, socket };
+  const client: Client = { id, socket, roomId: null };
   clients.set(id, client);
   console.log(`${id} connected!`);
 
@@ -46,16 +48,70 @@ wss.on("connection", (socket: WebSocket) => {
   );
 
   socket.on("message", (data) => {
-    const parsedData = data.toString();
-    console.log(`${id}: ${parsedData}`);
+    let parsedData: {
+      type: string;
+      roomName?: string;
+      message?: string;
+    };
 
-    broadcast(
-      JSON.stringify({
-        type: "message",
-        id: id,
-        message: parsedData,
-      }),
-    );
+    try {
+      parsedData = JSON.parse(data.toString());
+    } catch {
+      socket.send(
+        JSON.stringify({
+          type: "error",
+          message: "invalid json",
+        }),
+      );
+      return;
+    }
+
+    const client = clients.get(id);
+
+    switch (parsedData.type) {
+      case "join": {
+        if (!parsedData.roomName) {
+          socket.send(
+            JSON.stringify({
+              type: "error",
+              message: "roomname is required",
+            }),
+          );
+          break;
+        }
+        const roomName = parsedData.roomName;
+
+        if (!rooms.has(roomName)) {
+          rooms.set(roomName, new Set<string>());
+        }
+
+        rooms.get(roomName)!.add(id);
+        // const client = clients.get(id);
+        client.roomId = roomName;
+
+        socket.send(
+          JSON.stringify({
+            type: "joined",
+            roomName,
+            message: `welcome to ${roomName}!`,
+          }),
+        );
+
+        for (const clientId of rooms.get(roomName)!) {
+          if (clientId === id) continue;
+          const member = clients.get(clientId);
+          if (member && member.socket.readyState === WebSocket.OPEN) {
+            member.socket.send(
+              JSON.stringify({
+                type: "user_joined",
+                from: id,
+                message: `${id} has joined the ${roomName} room`,
+              }),
+            );
+          }
+        }
+      }
+    }
   });
 
   socket.on("close", () => {
